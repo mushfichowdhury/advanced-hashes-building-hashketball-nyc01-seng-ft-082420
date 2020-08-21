11d9af8d2cc3b39723a57ecbedac8c9c7851dfca
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-08-21 11:26:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,14 @@
-# Write your code here!
+def game_hash
+  hash = {
+    :home => {
+      :team_name => ["Brooklyn Nets"],
+      :colors => ["Black", "White"],
+      :players => [
+        ]
+    }
+    :away => {
+      :team_name
+      :colors
+      :players
+    }
+  }
