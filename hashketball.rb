a4b62fb2b5c066e6145641dd5cb2ab016033f944
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-08-21 11:40:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -53,7 +53,7 @@ def game_hash
     },
     :away => {
       :team_name => "Charlotte Hornets",
-      :colors => "Turqoise", "Purple",
+      :colors => ["Turqoise", "Purple"],
       :players => [
         {:player_name => "Jeff Adrien", 
         :number => 4, 
